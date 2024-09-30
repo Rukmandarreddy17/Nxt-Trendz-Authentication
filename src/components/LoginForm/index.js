77ff89a12276cd483a990835dba70c1b4a2a2e52
Rukmandarreddy <urukmandarreddy@gmail.com>
2024-09-30 13:04:08 +0000

    Solution

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class LoginForm extends Component {
@@ -6,104 +7,107 @@ class LoginForm extends Component {
     username: '',
     password: '',
     errorMsg: '',
-    errorMsg1: '',
-    errorMsg2: '',
   }
 
   onSubmitSuccess = () => {
     const {history} = this.props
     history.replace('/')
   }
+  onSubmitFailure = errorMsg => {
+    this.setState({errorMsg: errorMsg})
+  }
 
   submitForm = async event => {
     event.preventDefault()
     const {username, password} = this.state
-
-    this.setState({errorMsg: '', errorMsg1: '', errorMsg2: ''})
-
-    if (username === '') {
-      this.setState({errorMsg1: '*Username should not be empty'})
-      return
-    }
-
-    if (password === '') {
-      this.setState({errorMsg2: '*Password should not be empty'})
-      return
-    }
-
     const userDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
-      headers: {
-        'Content-Type': 'application/json',
-      },
     }
-
-    const response = await fetch('https://apis.ccbp.in/login', options)
+    const response = await fetch(url, options)
     const data = await response.json()
-
-    if (response.ok) {
+    if (response.ok === true) {
       this.onSubmitSuccess()
     } else {
-      this.setState({errorMsg: "*Username and Password didn't match"})
+      this.onSubmitFailure(data.error_msg)
     }
   }
 
-  updateUsername = event => {
+  onChangeUsername = event => {
     this.setState({username: event.target.value})
   }
 
-  updatePassword = event => {
+  onChangePassword = event => {
     this.setState({password: event.target.value})
   }
 
-  render() {
-    const {username, password, errorMsg, errorMsg1, errorMsg2} = this.state
+  renderPasswordField = () => {
+    const {password} = this.state
     return (
-      <div className="bgContainer">
-        <div className="c1">
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+          palceholder="Password"
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+          palceholder="Username"
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {errorMsg} = this.state
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.submitForm}>
           <img
-            className="websiteLoginImg"
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
-            alt="website login"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
           />
-          <form className="fromContainer" onSubmit={this.submitForm}>
-            <img
-              className="nxtTrendzLogo"
-              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-              alt="website logo"
-            />
-            <label htmlFor="username" className="labelElement">
-              USERNAME
-            </label>
-            <input
-              onChange={this.updateUsername}
-              className="inputElement"
-              id="username"
-              value={username}
-              type="text"
-              placeholder="Username"
-            />
-            <p className="errormsg">{errorMsg1}</p>
-            <label htmlFor="password" className="labelElement">
-              PASSWORD
-            </label>
-            <input
-              onChange={this.updatePassword}
-              className="inputElement"
-              id="password"
-              value={password}
-              type="password"
-              placeholder="Password"
-            />
-            <p className="errormsg">{errorMsg2}</p>
-            <button className="loginButton" type="submit">
-              Login
-            </button>
-            <p className="errormsg">{errorMsg}</p>
-          </form>
-        </div>
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          <p className="errorMsg">{errorMsg}</p>
+        </form>
       </div>
     )
   }
